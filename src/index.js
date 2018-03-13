a9b69bd40aa07a1c720a5789872f761993029789
Erik Gelderblom <hello@erikgelderblom.com>
2018-03-13 15:38:03 +0100

    added keyboard nov

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const canvas = document.querySelector('canvas');
 const canvasWidth = canvas.getBoundingClientRect().width;
 const canvasHeight = canvas.getBoundingClientRect().height;
 
+const wrapper = document.querySelector('.wrapper');
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(
   window.getComputedStyle(document.body).backgroundColor
@@ -21,12 +23,7 @@ camera.lookAt(scene.position);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-renderer.setSize(canvasWidth, canvas.getBoundingClientRect().height);
-
-/* Set up scene */
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
+renderer.setSize(canvasWidth, canvasHeight);
 
 // material
 const material = new THREE.MeshBasicMaterial({
@@ -57,6 +54,8 @@ const tanFOV = Math.tan(Math.PI / 180 * camera.fov / 2);
 const windowHeight = window.innerHeight;
 
 function onWindowResize(event) {
+  const isMobile = window.matchMedia('(max-width: 80ch)').matches;
+  const wrapperWidth = wrapper.getBoundingClientRect().width;
   camera.aspect = window.aspect = window.innerWidth / window.innerHeight;
   camera.fov =
     360 / Math.PI * Math.atan(tanFOV * (window.innerHeight / windowHeight));
@@ -64,8 +63,100 @@ function onWindowResize(event) {
   camera.updateProjectionMatrix();
   camera.lookAt(scene.position);
 
-  renderer.setSize(canvasWidth, canvasHeight);
+  console.log(isMobile);
+  const rWidth = isMobile
+    ? window.innerWidth
+    : window.innerWidth - wrapperWidth;
+  const rHeight = isMobile ? window.innerHeight / 2 : window.innerHeight;
+
+  renderer.setSize(rWidth, rHeight);
+
   renderer.render(scene, camera);
 }
 
 window.addEventListener('resize', onWindowResize);
+
+/**
+ * Minimal Keyboard navigation
+ */
+const contactNav = document.querySelectorAll('.contact a');
+
+function navigate(el) {
+  window.location = el.href;
+}
+
+const getFocusIndex = () => {
+  const index = [...contactNav].indexOf(document.activeElement);
+  return index >= 0 ? index : 0;
+};
+
+const isInContactNav = el => [...contactNav].includes(el);
+
+function setFocus(keyCode) {
+  const totalItems = contactNav.length - 1;
+  const currFocus = isInContactNav(document.activeElement)
+    ? getFocusIndex()
+    : -1;
+  let nextFocus;
+  let shouldRepeat;
+
+  if (keyCode === 37) {
+    shouldRepeat = currFocus <= 0;
+    nextFocus = shouldRepeat ? totalItems : currFocus - 1;
+  }
+
+  if (keyCode === 39) {
+    console.log(currFocus);
+    shouldRepeat = currFocus >= totalItems;
+    nextFocus = shouldRepeat ? 0 : currFocus + 1;
+  }
+
+  console.log(currFocus);
+  return contactNav[nextFocus];
+}
+
+window.addEventListener('keydown', e => {
+  let target = contactNav;
+
+  switch (e.keyCode) {
+    // 1
+    case 49:
+      contactNav[0].focus();
+      break;
+
+    // 2
+    case 50:
+      contactNav[1].focus();
+      break;
+
+    // 3
+    case 51:
+      contactNav[2].focus();
+      break;
+
+    // 4
+    case 52:
+      contactNav[3].focus();
+      break;
+
+    // Left arrow
+    case 37:
+      target = setFocus(e.keyCode);
+      target.focus();
+      break;
+
+    // Right arrow
+    case 39:
+      console.log(contactNav[1]);
+      target = setFocus(e.keyCode);
+      target.focus();
+      break;
+
+    // Enter
+    case 13:
+      navigate(document.activeElement);
+      break;
+
+    default:
+  }
+});
