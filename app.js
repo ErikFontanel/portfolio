27a272efe538a8a32620ae52a44b7cfc024a3a0f
Erik Gelderblom <hello@erikgelderblom.com>
2018-10-16 07:51:04 +0200

    updated imgs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const $ = selector => document.querySelector(selector);
 const getGallery = urls => {
   if (urls.length > 0) {
     const htmlstring = urls.map(url => {
-      return `<img src="/${url}"/>`;
+      return `<figure><img src="/${url}"/></figure>`;
     });
     return htmlstring.join('');
   }
