baff24715e79b4707a9ae290bd3ef6152cfd41cb
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-01-17 16:03:58 +0100

    remove projects

--- app.js
+++ app.js
@@ -1,62 +1,9 @@
-import slugify from 'slugify';
-import projects from '~/content/projects';
-import './src/intro';
+import slugify from "slugify";
+import projects from "~/content/projects";
+import "./src/intro";
 
-const projectsContainer = document.createDocumentFragment();
-const nav = document.createRange().createContextualFragment('<ol></ol>');
+const nav = document.createRange().createContextualFragment("<ol></ol>");
 const $ = selector => document.querySelector(selector);
-
-const getGallery = urls => {
-  if (urls.length > 0) {
-    const htmlstring = urls.map(url => {
-      return `<figure class="${
-        url.search(/\D\.png$/g) >= 0 ? 'mobile' : 'desktop'
-      }"><img src="/${url}"/></figure>`;
-    });
-    return htmlstring.join('');
-  }
-};
-
-const renderProject = node => {
-  const {
-    title,
-    description,
-    images,
-    collaboration,
-    services,
-    date,
-    url
-  } = node;
-
-  const id = slugify(title);
-  const web = url ? `<br><a href="${url}">Visit</a>` : '';
-  const year = date ? `Year: ${date}<br>` : '';
-  const collab = collaboration ? `With: ${collaboration}<br>` : '';
-  const service = services ? `Services: ${services.join(', ')}<br>` : '';
-
-  const gallery = getGallery(images);
-
-  const html = `
-  <article id="${id}" class="project">
-    <div class="project-info">
-      <h1 class="project-title listheader">${title}</h1>
-      <p>${description}</p>
-      <p class="project-meta">
-      ${service}
-      ${collab}
-      ${year}
-      ${web}
-      </p>
-    </div>
-
-    <div class="project-gallery">
-      ${gallery}
-    </div>
-  </article>
-  `;
-
-  return document.createRange().createContextualFragment(html);
-};
 
 const renderNavItem = node => {
   const html = `
@@ -68,13 +15,10 @@ const renderNavItem = node => {
 
 if (projects !== undefined) {
   projects.forEach(project => {
-    const node = renderProject(project);
     const navItem = renderNavItem(project);
 
-    nav.querySelector('ol').appendChild(navItem);
-    projectsContainer.appendChild(node);
+    nav.querySelector("ol").appendChild(navItem);
   });
 
-  $('#selected-work').appendChild(nav);
-  $('.projects').appendChild(projectsContainer);
+  $("#selected-work").appendChild(nav);
 }
