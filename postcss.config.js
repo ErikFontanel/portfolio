03c4180527a2bf390d24f7e66ba9d3de63c880d3
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-06-22 13:01:44 +0200

    add postcss-media-minmax

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,16 @@ const isDev = () => process.env.NODE_ENV !== 'production';
 
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({
-      autoprefixer: !isDev()
-        ? {
-            grid: true,
-          }
-        : false,
-    }),
-    ,
+    !isDev()
+      ? require('postcss-preset-env')({
+          autoprefixer: !isDev()
+            ? {
+                grid: true,
+              }
+            : false,
+        })
+      : false,
+    require('postcss-media-minmax'),
     require('postcss-custom-media'),
   ],
 };
