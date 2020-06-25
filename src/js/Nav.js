bb5a88db28bf8b3a3a3402c56984685166b3ad01
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-06-25 15:04:30 +0200

    add close nav on click overlay

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -2,19 +2,22 @@ export default class Nav {
   constructor(el) {
     this.el = el;
     this.visible = false;
-    [...this.el.querySelectorAll('[aria-controls]')].map((btn) =>
-      btn.addEventListener('click', this.toggleNav.bind(this))
-    );
+
+    // [...this.el.querySelectorAll('[aria-controls]')].map((btn) =>
+    //   btn.addEventListener('click', this.toggleNav.bind(this))
+    //   );
+    this.el.addEventListener('click', this.toggleNav.bind(this));
   }
 
   toggleNav() {
-    const btn = event.currentTarget;
-    const target = this.el.querySelector(btn.getAttribute('aria-controls'));
+    const nav = event.currentTarget;
+    const btn = nav.querySelector('[aria-controls]');
+    const popover = nav.querySelector(btn.getAttribute('aria-controls'));
     event.preventDefault();
 
     btn.setAttribute('aria-expanded', !this.visible);
-    this.el.setAttribute('data-showing-overlay', !this.visible);
-    target.setAttribute('aria-hidden', this.visible);
+    nav.setAttribute('data-showing-overlay', !this.visible);
+    popover.setAttribute('aria-hidden', this.visible);
     this.visible = !this.visible;
   }
 }
