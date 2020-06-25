1d80516f46112ed6a362b395d70d0ec30e4f3788
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-06-25 14:52:53 +0200

    more button styles

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -1,7 +1,7 @@
 export default class Nav {
   constructor(el) {
     this.el = el;
-
+    this.visible = false;
     [...this.el.querySelectorAll('[aria-controls]')].map((btn) =>
       btn.addEventListener('click', this.toggleNav.bind(this))
     );
@@ -12,9 +12,9 @@ export default class Nav {
     const target = this.el.querySelector(btn.getAttribute('aria-controls'));
     event.preventDefault();
 
-    btn.toggleAttribute('aria-expanded');
-    this.el.toggleAttribute('data-showing-overlay');
-    target.toggleAttribute('aria-hidden');
+    btn.setAttribute('aria-expanded', !this.visible);
+    this.el.setAttribute('data-showing-overlay', !this.visible);
+    target.setAttribute('aria-hidden', this.visible);
     this.visible = !this.visible;
   }
 }
