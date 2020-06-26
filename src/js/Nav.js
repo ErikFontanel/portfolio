73f4c7a208305c6e7c3186579656abf5db0fe64e
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-06-26 15:11:24 +0200

    stop body scroll

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -4,17 +4,23 @@ export default class Nav {
     this.visible = false;
 
     this.el.addEventListener('click', this.toggleNav.bind(this));
+    window.scrollTo(0, 1);
   }
 
   toggleNav() {
     const nav = event.currentTarget;
     const btn = nav.querySelector('[aria-controls]');
     const popover = nav.querySelector(btn.getAttribute('aria-controls'));
-    if (!event.target.hasAttribute('href')) event.preventDefault();
+    // event.preventDefault();
+    event.stopPropagation();
+    if (!event.target.hasAttribute('href') || event.target === btn)
+      event.preventDefault();
 
     btn.setAttribute('aria-expanded', !this.visible);
     nav.setAttribute('data-showing-overlay', !this.visible);
     popover.setAttribute('aria-hidden', this.visible);
+    document.body.setAttribute('data-showing-overlay', !this.visible);
+
     this.visible = !this.visible;
   }
 }
