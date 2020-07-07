0846d11b07485a85ff8be5e1aacdd5e79b82106c
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-07-07 15:35:45 +0200

    no unused vars

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ import Carousel from './js/carousel';
 document.documentElement.classList.remove('no-js');
 
 if (import.meta.hot) {
-  import.meta.hot.accept(({ module }) => {
+  import.meta.hot.accept(() => {
     // Accept the module, apply it to your application.
     console.log('hot reloading…');
   });
+
   import.meta.hot.dispose(() => {
     // Cleanup any side-effects. Optional.
   });
