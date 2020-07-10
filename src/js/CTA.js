52dc33f2d1ebda85369b05a099f615f3b075d2a8
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-07-10 13:51:52 +0200

    add sticky cta

---
+++ src/js/CTA.js
@@ -0,0 +1,13 @@
+import { debounce } from 'lodash-es';
+
+export default class CTA {
+  constructor(el) {
+    this.el = el;
+    window.addEventListener('scroll', debounce(this.onScroll.bind(this)));
+
+  }
+
+  onScroll() {
+    this.el.toggleAttribute('data-is-scrolling', window.scrollY);
+  }
+}
