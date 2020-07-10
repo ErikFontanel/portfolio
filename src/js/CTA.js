5a37cd596b5532bddbae53f13284e8e241be1102
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-07-10 13:56:35 +0200

    remove three for now

--- src/js/CTA.js
+++ src/js/CTA.js
@@ -1,13 +1,9 @@
-// import Gl from './Gl';
 import { debounce } from 'lodash-es';
 
 export default class CTA {
   constructor(el) {
     this.el = el;
     window.addEventListener('scroll', debounce(this.onScroll.bind(this)));
-
-    // const gl = new Gl(this.el);
-    // gl.init();
   }
 
   onScroll() {
