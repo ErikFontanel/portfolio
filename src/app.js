f7a1e588bcbd8e43bc1ff976db374a5acc5566f3
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-07-10 13:53:44 +0200

    add cta sticky styling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './css/app.css';
 import Nav from './js/Nav';
 import Carousel from './js/carousel';
+import CTA from './js/CTA';
 
 document.documentElement.classList.remove('no-js');
 
@@ -16,8 +17,10 @@ if (import.meta.hot) {
 }
 
 const navMain = document.querySelector('.site-header');
+const cta = document.querySelector('.cta');
 
-new Nav(navMain);
+if (navMain) new Nav(navMain);
+if (cta) new CTA(cta);
 
 [...document.querySelectorAll('.carousel')].map(
   (el) => new Carousel({ el: el })
