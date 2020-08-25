47c45e4262a8e2c14baaabff1c92386e4befefca
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-08-25 16:08:21 +0200

    fix postcss / sass live reload

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,23 +6,36 @@ module.exports = {
     },
   },
   scripts: {
-    'run:sass': 'sass src/scss:src/scss --no-source-map',
-    'run:sass::watch': '$1 --watch',
-    'run:tpostcss': 'postcss src/scss/**/*.css',
-    'run:tpostcss::watch': 'postcss src/scss/**/*.css -d src/css --watch',
-    'run:11ty': 'eleventy',
+    // 'run:sass': 'sass src/scss:src/scss --no-source-map',
+    // 'run:sass::watch': '$1 --watch',
+    // 'run:tpostcss': 'postcss src/scss/**/*.css',
+    // 'run:tpostcss::watch': 'postcss src/scss/**/*.css -d src/css --watch',
+    'run:11ty': 'eleventy --quiet',
     'run:11ty::watch': '$1 --watch',
   },
   mount: {
     dist: '/',
     src: '/public',
   },
-  // plugins: [
-  //   [
-  //     '@snowpack/plugin-run-script',
-  //     { cmd: 'sass src/scss:public/css --no-source-map', watch: '$1 --watch' },
-  //   ],
-  // ],
+  plugins: [
+    [
+      '@snowpack/plugin-run-script',
+      { cmd: 'sass src/scss:src/css', watch: '$1 --watch' },
+    ],
+    [
+      '@snowpack/plugin-run-script',
+      { cmd: 'postcss src/scss/**/*.css -d src/css', watch: '$1 --watch' },
+    ],
+    [
+      '@snowpack/plugin-build-script',
+      {
+        cmd: 'postcss',
+        input: ['src/scss/*.css'],
+        output: ['src/css/'],
+        watch: '$1 --watch',
+      },
+    ],
+  ],
   devOptions: {
     port: 8080,
     open: 'none',
