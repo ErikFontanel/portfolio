8b040514b0817241894b3d6c49ad8f81ce1706c5
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-09-22 15:36:23 +0200

    add responsive image snippet for netlify

--- _includes/components/Image.js
+++ _includes/components/Image.js
@@ -6,7 +6,7 @@ const attrs = (attrs) =>
     return previous + ` ${current}="${Object.values(attrs)[idx]}"`;
   }, []);
 
-module.exports = ({ url, alt, css, lazy, context }) => {
+module.exports = ({ url, alt, css, width, height, lazy, context }) => {
   // const file = path.resolve(__dirname, `../../content/${context}/${url}`);
 
   // const { width, height } = file ? sizeOf(file) : false;
@@ -14,10 +14,25 @@ module.exports = ({ url, alt, css, lazy, context }) => {
   const attributes = attrs({
     ...(alt && { alt: alt }),
     ...(css && { class: css }),
-    // ...{ loading: lazy ?? 'lazy' },
-    // ...(width && { width: width }),
-    // ...(height && { height: height }),
+    ...{ loading: lazy ?? 'lazy' },
+    ...(width && { width: width }),
+    ...(height && { height: height }),
   });
 
-  return `<img src="${url}" ${attributes} />`;
+  const scales = [2, 0.5];
+
+  const srcset = scales.map((size) => {
+    const w = Math.floor(width / size);
+    const h = Math.floor(height / size);
+
+    if (w === Infinity || h === Infinity) return false;
+
+    return `${url}?nf_resize=smartcrop&w=${w}&h=${h} ${w}w`;
+  });
+
+  const sizes = '(min-width: 64rem) 50vw, 100vw';
+
+  return `<img src="${url}" sizes="${sizes}" srcset="${srcset.join(
+    ', '
+  )}" ${attributes} />`;
 };
