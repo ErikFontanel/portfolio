88a8742c62fa510b4945a1690c0a88d75f9deb9f
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-09-22 16:00:49 +0200

    only add srcset if width present

--- _includes/components/Image.js
+++ _includes/components/Image.js
@@ -6,7 +6,7 @@ const attrs = (attrs) =>
     return previous + ` ${current}="${Object.values(attrs)[idx]}"`;
   }, []);
 
-module.exports = ({ url, alt, css, width, height, lazy, context }) => {
+module.exports = ({ url, alt, css, width, height, sizes, lazy, context }) => {
   // const file = path.resolve(__dirname, `../../content/${context}/${url}`);
 
   // const { width, height } = file ? sizeOf(file) : false;
@@ -18,21 +18,23 @@ module.exports = ({ url, alt, css, width, height, lazy, context }) => {
     ...(width && { width: width }),
     ...(height && { height: height }),
   });
-if(width){
+
   const scales = [2, 0.5];
 
   const srcset = scales.map((size) => {
-    const w = Math.floor(width / size);
-    const h = Math.floor(height / size);
+    if (width) {
+      const w = Math.floor(width / size);
+      const h = Math.floor(height / size);
 
-    if (w === Infinity || h === Infinity) return false;
+      if (w === Infinity || h === Infinity) return false;
 
-    return `${url}?nf_resize=smartcrop&w=${w}&h=${h} ${w}w`;
+      return `${url}?nf_resize=smartcrop&w=${w}&h=${h} ${w}w`;
+    }
+
+    sizes = sizes ? sizes : '(min-width: 64rem) 50vw, 100vw';
   });
 
-  const sizes = '(min-width: 64rem) 50vw, 100vw';
-}
-  return `<img src="${url}" sizes="${sizes ? '100vw'}" srcset="${srcset ? srcset.join(
-    ', '
-  ) : url}" ${attributes} />`;
+  return `<img src="${url}" sizes="${sizes ? sizes : '100vw'}" srcset="${
+    srcset ? srcset.join(', ') : url
+  }" ${attributes} />`;
 };
