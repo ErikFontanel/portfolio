eb0ae357b5c889a412f8330b691307b247aabc07
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2020-11-13 18:04:57 +0100

    fix error

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,18 @@
-const isProduction = () => process.env.NODE_ENV === 'production';
-
-module.exports = {
-  map: false,
-  plugins: [
-    require('postcss-media-minmax'),
-    require('postcss-custom-media'),
-    ...(isProduction()
-      ? require('postcss-preset-env')({
-          autoprefixer: isProduction()
-            ? {
-                grid: true,
-              }
-            : false,
-        })
-      : []),
-    ...(isProduction()
-      ? require('cssnano')({
-          preset: 'default',
-        })
-      : []),
-  ],
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    map: false,
+    plugins: [
+      require('postcss-media-minmax'),
+      require('postcss-custom-media'),
+      require('postcss-preset-env'),
+      require('cssnano')({
+        preset: 'default',
+      }),
+    ],
+  };
+} else {
+  module.exports = {
+    map: false,
+    plugins: [require('postcss-media-minmax'), require('postcss-custom-media')],
+  };
+}
