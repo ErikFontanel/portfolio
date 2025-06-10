2a5e5a8249a4be642aa515397e8824b194f2bb5e
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2025-06-10 11:30:26 +0200

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,13 @@ import postcssMediaMinmax from 'postcss-media-minmax';
 import postcssCustomMedia from 'postcss-custom-media';
 import postcssPresetEnv from 'postcss-preset-env';
 import cssnano from 'cssnano';
-
+import autoprefixer from 'autoprefixer';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   map: false,
   plugins: [
+    autoprefixer,
     postcssMediaMinmax,
     postcssCustomMedia,
     ...(isProduction
