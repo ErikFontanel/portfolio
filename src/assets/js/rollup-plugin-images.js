501548e88af770be06ef823f47cf96c82fcf76ae
Erik Gelderblom <ErikFontanel@users.noreply.github.com>
2025-08-27 19:53:52 +0200

    fix for netlify

--- src/assets/js/rollup-plugin-images.js
+++ src/assets/js/rollup-plugin-images.js
@@ -61,8 +61,7 @@ export default function htmlImgDimensions() {
           }
 
           // Update bundle with modified HTML
-          // file.source = dom.serialize();
-          fs.writeFileSync(htmlPath, dom.serialize(), 'utf-8');
+          file.source = dom.serialize();
         }
       }
     },
